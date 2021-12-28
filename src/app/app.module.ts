02f9a08ae1b9cc448699c600291c49d65924a351
Martín <magadiflo@gmail.com>
2021-12-28 09:54:47 -0500

    14. PrimeNg - Fieldset

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -18,6 +19,7 @@ registerLocaleData(localeEsPe);
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRouterModule,
     SharedModule,
     VentasModule,
