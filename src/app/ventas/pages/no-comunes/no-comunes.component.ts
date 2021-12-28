7df2925751d6627400457922a9391d5c15f688cd
Martín <magadiflo@gmail.com>
2021-12-28 10:13:23 -0500

    16. i18nPlural Pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -8,12 +8,22 @@ import { Component } from '@angular/core';
 })
 export class NoComunesComponent {
 
+  //i18nSelect
   nombre: string = 'Martín';
   genero: string = 'masculino';
 
   invitacionMapa = {
     'masculino' : 'invitarlo',
     'femenino' : 'invitarla'
+  }
+
+  //i18nPlural
+  clientes: string[] = ['María', 'Pedro', 'Hernando', 'Eduardo', 'Fernando'];
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    'other': 'tenemos # clientes esperando.'
   }
 
 }
