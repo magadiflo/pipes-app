21009039f0cdff15b353a18e19ae1fafface96f9
Martín <magadiflo@gmail.com>
2023-07-03 23:19:50 -0500

    Navegar usando el MenuBar

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -17,15 +17,18 @@ export class MenuComponent implements OnInit {
         items: [
           {
             label: 'Textos y Fechas',
-            icon: 'pi pi-align-left'
+            icon: 'pi pi-align-left',
+            routerLink: ['/basics']
           },
           {
             label: 'Números',
-            icon: 'pi pi-dollar'
+            icon: 'pi pi-dollar',
+            routerLink: ['/numbers']
           },
           {
             label: 'No comunes',
-            icon: 'pi pi-globe'
+            icon: 'pi pi-globe',
+            routerLink: ['/uncommon']
           },
         ]
       },
