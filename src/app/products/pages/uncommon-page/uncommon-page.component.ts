399690e927f617ae22220ee64973eb2b278f9172
Martín <magadiflo@gmail.com>
2023-07-13 23:06:45 -0500

    i18nPluralPipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
-  //i18n Select
+  //i18nSelect Pipe --------------------------------
   public name: string = 'Martín';
   public gender: 'male' | 'female' = 'male';
   public invitationMap = {
@@ -15,9 +15,22 @@ export class UncommonPageComponent {
     'female': 'invitarla',
   }
 
-  changeClient() {
+  changeClient():void {
     this.name = 'María';
     this.gender = 'female';
+  }
+
+  //i18nPlural Pipe ---------------------------------
+  public clients: String[] = ['Martín', 'María', 'Mercedes', 'Mirtha'];
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    'other': 'tenemos # clientes esperando.'
+  }
+
+  removeClient(): void {
+    this.clients.splice(0, 1);
   }
 
 }
