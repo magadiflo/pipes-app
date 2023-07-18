b4cfc78187cc00e77f69407244fec1cf53405d0c
Martín <magadiflo@gmail.com>
2023-07-17 23:09:10 -0500

    PrimeNG - Table

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../interfaces/product.interface';
 
 @Component({
   selector: 'products-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.scss']
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
 
   public isUpperCase: boolean = false;
+  public products!: Product[];
 
   toggleUpperCase(): void {
     this.isUpperCase = !this.isUpperCase;
+  }
+
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.productService.getProducts()
+      .then((data) => {
+        this.products = data;
+      });
   }
 
 }
